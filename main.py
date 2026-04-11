615c56b27d69c690bbc29f05d584c1808f1f2c84
DanialTech2012 <toktarasan7@gmail.com>
2026-04-11 16:30:39 +0500

    2 endpoints

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import Response
+
+app = FastAPI()
+
+BALANCE = {
+    "cash" : 10000,
+    "cash2" : 15000
+}
+
+@app.get("/health")
+def health_checker():
+    return Response(status_code=200)
+
+@app.get("/balance")
+def get_balance(wallet_name : str | None = None):
+    if wallet_name is None:
+        return Response(status_code=200,
+                        content=f"Your balance across all wallets is {sum(BALANCE.values())}")
+
+    if wallet_name not in BALANCE:
+        return HTTPException(status_code=400,
+                             detail=f"Wallet with name {wallet_name} is not found")
+    
+    return {"wallet_name" : wallet_name,
+            "balance" : BALANCE[wallet_name]}
+
+
+
+
+@app.post("/wallet/{wallet_name}")
+def reseive_money(wallet_name : str, amount : int):
+    if wallet_name not in BALANCE:
+        BALANCE[wallet_name] = 0
+        return {"detail": f"Wallet with {wallet_name} is created"}
+    
+    BALANCE[wallet_name] += amount
+
+    return {"wallet_name": {wallet_name},
+            "detail" : f"Your wallet {wallet_name} is top up with {amount}"}
+
